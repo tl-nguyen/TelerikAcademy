a9cf63aab79a1c0496944da260a970a319211633
TL Nguyen <tl.nguyen.1987@gmail.com>
2014-06-22 12:38:08 +0300

    Refactoring HW
    there are still a lot of things to do

---
+++ 06.HighQualityCode/12.Matrix.Tests/WalkInMatrixTests/WalkInMatrixTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Matrix.Tests
+{
+    using System;
+    using Matrix;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class WalkInMatrixTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
