71c500bbdc300ced77a3fbdb72babeddc51cedc0
TL Nguyen <tl.nguyen.1987@gmail.com>
2014-08-30 23:08:41 +0300

    DBs - Code-First - HW

---
+++ 11.Databases/10.Entity-Framework-Code-First/StudentSystem.Data/StudentSystemDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace StudentSystem.Data
+{
+    using System.Data.Entity;
+
+    using StudentSystem.Model;
+    using StudentSystem.Data.Migrations;
+
+    public class StudentSystemDbContext : DbContext
+    {
+        public StudentSystemDbContext()
+            : base("StudentSystemConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<StudentSystemDbContext, Configuration>());
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> Homeworks { get; set; }
+    }
+}
---
+++ 11.Databases/10.Entity-Framework-Code-First/StudentSystem.Model/Course.cs
@@ -0,0 +1,50 @@
+﻿namespace StudentSystem.Model
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Course
+    {
+        private ICollection<Student> students;
+        private ICollection<Homework> homeworks;
+
+        public Course()
+        {
+            this.students = new HashSet<Student>();
+            this.homeworks = new HashSet<Homework>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Materials { get; set; }
+
+        public virtual ICollection<Student> Students
+        {
+            get
+            {
+                return this.students;
+            }
+            set
+            {
+                this.students = value;
+            }
+        }
+
+        public virtual ICollection<Homework> Homeworks
+        {
+            get
+            {
+                return this.homeworks;
+            }
+            set
+            {
+                this.homeworks = value;
+            }
+        }
+    }
+}
