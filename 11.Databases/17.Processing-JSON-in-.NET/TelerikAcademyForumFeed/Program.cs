5876440219043dd950b6519419ca4b0d4798a7d7
TL Nguyen <tl.nguyen.1987@gmail.com>
2014-09-07 15:56:38 +0300

    Processing Json in .Net - tasks: 1,2,3,4

---
+++ 11.Databases/17.Processing-JSON-in-.NET/TelerikAcademyForumFeed/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace TelerikAcademyForumFeed
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using Newtonsoft.Json;
+    using System.Xml.Linq;
+    using Newtonsoft.Json.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webClient = new WebClient();
+            string address = "http://forums.academy.telerik.com/feed/qa.rss";
+            string filePath = @"..\..\..\TA-forum-feed.xml";
+
+            webClient.DownloadFile(address, filePath);
+
+            var xmlDoc = XDocument.Load(filePath);
+
+            string jsonDoc = JsonConvert.SerializeXNode(xmlDoc, Newtonsoft.Json.Formatting.Indented);
+
+            var titles = GetTitle(jsonDoc);
+
+            foreach (var title in titles)
+            {
+                Console.WriteLine(title);
+            }
+
+            var poco = ConvertToPoco(jsonDoc);
+
+            Console.WriteLine(poco);
+        }
+
+        private static object ConvertToPoco(string jsonDoc)
+        {
+            var template = new
+            {
+                Rss = new
+                {
+                    Channel = new
+                    {
+                        Title = "",
+                        Link = "",
+                        Description = ""
+                    }
+                }
+            };
+
+            var convertedObj = JsonConvert.DeserializeAnonymousType(jsonDoc, template);
+
+            return convertedObj;
+        }
+
+        private static ICollection<JToken> GetTitle(string jsonDoc)
+        {
+            var jsonObj = JObject.Parse(jsonDoc);
+
+            var titles = jsonObj["rss"]["channel"]["item"]
+                                            .Select(i => i["title"])
+                                            .ToList();
+
+            return titles;
+        }
+    }
+}
